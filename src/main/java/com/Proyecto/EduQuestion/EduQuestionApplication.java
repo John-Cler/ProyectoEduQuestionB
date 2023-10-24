e847ff4d036783f889068fdef485143798303586
@eco-intern <git-intern@econotec.com.bo>
2023-10-24 01:11:00 -0400

    Actualizando primer commit

---
+++ src/main/java/com/Proyecto/EduQuestion/EduQuestionApplication.java
@@ -0,0 +1,13 @@
+package com.Proyecto.EduQuestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduQuestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduQuestionApplication.class, args);
+	}
+
+}
